72790abd52291a932370bdf916980955745e35c4
sombriks <sombriks@gmail.com>
2019-04-05 18:31:55 -0300

    release

--- src/components/wl-card.vue
+++ src/components/wl-card.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="tile is-child box">a</div>
+  <div class="tile is-child box">
+    <div>{{ wl.dt }}</div>
+    <div>{{ wl.e1 }}</div>
+    <div>{{ wl.s1 || "-" }}</div>
+    <div>{{ wl.e2 || "-" }}</div>
+    <div>{{ wl.s2 || "-" }}</div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "wl-card"
+  name: "wl-card",
+  props: ["wl"]
 };
 </script>
 
